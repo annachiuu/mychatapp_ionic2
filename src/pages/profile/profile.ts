fa371d1fe4f9f645f0a451a7fa46179e9377257e
Anna Chiu <annachiu94@gmail.com>
2017-10-12 16:49:58 +0800

    Basic profile page created

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Profiles } from '../../models/profiles';
 
 @IonicPage()
 @Component({
@@ -9,11 +9,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
+  profile = {} as Profiles;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+  }
+
+  saveProfile(profile: profile) {
+    
   }
 
 }
